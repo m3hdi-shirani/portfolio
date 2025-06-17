58c6194efdb46d0b6a5f9878983b7b79d0839c38
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-06-17 13:01:59 +0330

    Implement page transitions using framer-motion for About, Contact, Portfolio, and Homepage; add TransitionProvider component for layout management.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { motion } from "framer-motion";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      About Page
+    </motion.div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      Contact Page
+    </motion.div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
