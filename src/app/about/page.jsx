c616cab42e531104076a5dc9b27443597c8147ae
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-06-17 14:52:22 +0330

    Add biography, skills, and experience sections to About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,7 +10,19 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      About Page
+      <div>
+        {/* Text */}
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+          {/* Bio */}
+          <div className="flex flex-col justify-center gap-12">Bio</div>
+          {/* Skills */}
+          <div>Skills</div>
+          {/* Experience */}
+          <div>Experience</div>
+        </div>
+        {/* Svg */}
+        <div className="hidden"></div>
+      </div>
     </motion.div>
   );
 };
