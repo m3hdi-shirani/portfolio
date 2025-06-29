84cdc97e7a41f35558a3f469a1ca06192171607f
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-06-29 14:58:23 +0330

    Enhance About page layout and add experience section Text

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,9 +10,9 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div>
+      <div className="h-full overflow-scroll lg:flex">
         {/* Text */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:1/2">
           {/* Bio */}
           <div className="flex flex-col justify-center gap-12">
             <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
@@ -162,10 +162,97 @@ const AboutPage = () => {
             </svg>
           </div>
           {/* Experience */}
-          <div>Experience</div>
+          <div className="flex flex-col justify-center gap-12 pb-48">
+            <h1 className="font-bold text-2xl">Experience</h1>
+            {/* Experience List */}
+            <div>
+              {/* Item */}
+              <div className="flex justify-between h-48">
+                {/* Left */}
+                <div className="w-1/3">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  <div className="p-3 text-sm italic">
+                    I led web development, offering expertise in JavaScript
+                    frameworks.{" "}
+                  </div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                  <div className="p-1 bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
+                </div>
+                {/* Center */}
+                <div className="w-1/6 ">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* Left */}
+                <div className="w-1/3"></div>
+              </div>
+              {/* Item 2 */}
+              <div className="flex justify-between h-48">
+                {/* Left */}
+                <div className="w-1/3"></div>
+                {/* Center */}
+                <div className="w-1/6 ">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* Left */}
+                <div className="w-1/3">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  <div className="p-3 text-sm italic">
+                    I led web development, offering expertise in JavaScript
+                    frameworks.{" "}
+                  </div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                  <div className="p-1 bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
+                </div>
+              </div>
+              {/* Item 3 */}
+              <div className="flex justify-between h-48">
+                {/* Left */}
+                <div className="w-1/3">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  <div className="p-3 text-sm italic">
+                    I led web development, offering expertise in JavaScript
+                    frameworks.{" "}
+                  </div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                  <div className="p-1 bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
+                </div>
+                {/* Center */}
+                <div className="w-1/6 ">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* Left */}
+                <div className="w-1/3"></div>
+              </div>
+            </div>
+          </div>
         </div>
+
         {/* Svg */}
-        <div className="hidden"></div>
+        <div className="hidden lg:block w-1/3 xl:1/2"></div>
       </div>
     </motion.div>
   );
